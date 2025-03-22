3e418ecf54945d7bb39a324f30b9f793ece82bf1
DEEPIKA200315 <147475708+DEEPIKA200315@users.noreply.github.com>
2025-03-22 08:25:38 -0700

    files added

---
+++ PORTFOLIO/javascript.js
@@ -0,0 +1,33 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () =>{
+  sections.forEach(sec =>{
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    if(top >= offset && top<offset + height){
+      navLinks.forEach.apply(links =>{
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('.active');
+      });
+    };
+  });
+
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky',window,scrolly >100);
+
+  menuIcon.classList.remove('fa-xmark');
+  navbar.classList.remove('active');
+}
+
+
+
